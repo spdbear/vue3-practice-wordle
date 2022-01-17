1eef473f4b120fa8f1842774f02746f7361646eb
spdbear <16793732+spdbear@users.noreply.github.com>
2022-01-17 13:27:29 +0900

    initial commit

---
+++ src/components/Button.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { ref, onMounted, reactive, readonly } from 'vue'
+const props = defineProps<{ text: string }>()
+const emit = defineEmits<{ (name: 'click', event: Event): void }>()
+
+const handleClick = (e: Event) => {
+  emit('click', e)
+}
+</script>
+
+<template>
+  <button
+    class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+    type="button"
+    @click="handleClick"
+  >
+    {{ text }}
+  </button>
+</template>
+
+<style scoped></style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { ref, onMounted, reactive, readonly } from 'vue'
+import Button from './Button.vue'
+
+defineProps<{ msg: string }>()
+
+const count = ref(0)
+
+const obj = reactive({ count })
+
+const objCopied = readonly(obj)
+
+onMounted(() => {
+  setInterval(() => count.value--, 1000)
+})
+</script>
+
+<template>
+  <div class="m-4">
+    <h1 class="mb-4 text-3xl font-bold underline">{{ msg }}</h1>
+    <Button :text="'Click'" @click="count++" />
+    <div class="m-4">
+      {{ obj.count }}
+    </div>
+    <Button :text="`count is: ${count}`" @click="obj.count++" />
+  </div>
+</template>
+
+<style scoped></style>
