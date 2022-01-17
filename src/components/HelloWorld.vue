a34787211a8b9420ed5aecf21548066a4a3b37eb
spdbear <16793732+spdbear@users.noreply.github.com>
2022-01-17 19:37:01 +0900

    TODOリストっぽいのを作る1

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 import { ref, onMounted, reactive, readonly } from 'vue'
 import Button from './Button.vue'
+import Todo from './Todo.vue'
+import Todo1 from './Todo.vue'
 
 defineProps<{ msg: string }>()
 
@@ -9,6 +11,19 @@ const count = ref(0)
 const obj = reactive({ count })
 
 const objCopied = readonly(obj)
+
+const todoList = [
+  {
+    text: 'やる',
+    time: 100,
+    checked: false,
+  },
+  {
+    text: 'やる2',
+    time: 200,
+    checked: false,
+  },
+]
 
 onMounted(() => {
   setInterval(() => count.value--, 1000)
@@ -22,6 +37,14 @@ onMounted(() => {
     <div class="m-4">
       {{ obj.count }}
     </div>
+    <template v-for="(todo, index) in todoList">
+      <Todo
+        :index="index"
+        :text="todo.text"
+        :time="todo.time"
+        :initial-checked="todo.checked"
+      ></Todo>
+    </template>
     <Button :text="`count is: ${count}`" @click="obj.count++" />
   </div>
 </template>
---
+++ src/components/Todo.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import { ref, computed } from 'vue'
+const props = defineProps<{
+  index: number
+  text: string
+  time: number
+  initialChecked: boolean
+}>()
+const emit = defineEmits<{ (name: 'click', event: Event): void }>()
+
+const checked = ref(props.initialChecked)
+
+const id = computed(() => props.index.toString())
+
+const handleClick = (e: Event) => {
+  emit('click', e)
+}
+</script>
+
+<template>
+  <div
+    class="flex items-center mt-2 mb-2 p-2 border-solid border-2 border-blue-500 rounded"
+  >
+    <input :id="id" type="checkbox" v-model="checked" />
+    <label :for="id" class="flex">
+      <div class="block ml-1">
+        {{ text }}
+      </div>
+      <div class="block ml-1">
+        {{ time }}
+      </div>
+    </label>
+  </div>
+</template>
+
+<style scoped></style>
