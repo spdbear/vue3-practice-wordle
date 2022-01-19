c8944fe2b61a46c2d03c7da822769c87c86d4ccd
spdbear <16793732+spdbear@users.noreply.github.com>
2022-01-19 10:59:04 +0900

    バリデーションを追加

--- src/components/TextBox.vue
+++ src/components/TextBox.vue
@@ -9,6 +9,9 @@ const emit = defineEmits<{ (name: 'change', updatedText: string): void }>()
 
 const texts = ref(props.initialText)
 
+const hasNoTextError = computed(() => texts.value.length === 0)
+const hasLongTextError = computed(() => texts.value.length > 10)
+
 const handleChange = (e: Event) => {
   const { target } = e
   if (!(target instanceof HTMLInputElement)) return
@@ -19,12 +22,22 @@ const handleChange = (e: Event) => {
 <template>
   <div>
     <input
-      class="bg-gray-100 focus:bg-white p-2 border-2 border-gray-500 shadow-md rounded text-gray-700"
+      :class="`bg-gray-100 focus:bg-white p-2 border-2 border-gray-500 shadow-md rounded text-gray-700 ${
+        (hasNoTextError || hasLongTextError) && 'error'
+      }`"
       v-model="texts"
       placeholder="input here"
       @change="handleChange"
     />
+    <div class="text-red-400" v-if="hasNoTextError">Please input text.</div>
+    <div class="text-red-400" v-if="hasLongTextError">
+      Text is limited to 10 characters.
+    </div>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.error {
+  @apply border-red-500 bg-red-100 focus:bg-red-100;
+}
+</style>
