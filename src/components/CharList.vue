0c34d3496a7f196d4a970a06cfd4496264ff6e97
spdbear <16793732+spdbear@users.noreply.github.com>
2022-01-26 18:54:40 +0900

    修正

--- src/components/CharList.vue
+++ src/components/CharList.vue
@@ -9,14 +9,22 @@ const props = defineProps<{
 
 const charArray = computed(() => props.charStr.split(''))
 const wordArray = computed(() => props.wordList.join('').split(''))
+const answerArray = computed(() => props.answer.split('')
 
 const getColor = (char: string) => {
   let isUsed = false
+  let isAnswer = false
   wordArray.value.forEach((usedChar: string) => {
     if (char === usedChar) {
       isUsed = true
     }
   })
+  answerArray.value.forEach((AnswerChar: string) => {
+    if (char === AnswerChar) {
+      isAnswer = true
+    }
+  })
+  if (isUsed && isAnswer) return 'yellow'
   if (isUsed) return 'gray'
   return 'white'
 }
@@ -27,6 +35,13 @@ const getColor = (char: string) => {
     <template v-for="(char, i) in charArray">
       <template v-if="getColor(char) === 'gray'">
         <div class="m-1 bg-gray-300 px-3 py-2 align-middle font-mono uppercase">
+          {{ char }}
+        </div>
+      </template>
+      <template v-else-if="getColor(char) === 'yellow'">
+        <div
+          class="m-1 bg-yellow-300 px-3 py-2 align-middle font-mono uppercase"
+        >
           {{ char }}
         </div>
       </template>
